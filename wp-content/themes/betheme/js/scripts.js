80ac17f67c83e085f89adc62921d68b70c8a24fb
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-03-24 23:19:43 -0700

    Increase to 7 seconds

--- wp-content/themes/betheme/js/scripts.js
+++ wp-content/themes/betheme/js/scripts.js
@@ -4238,7 +4238,7 @@ function msnryGalleryInit() {
 
         rtl: rtl ? true : false,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 7000,
 
         slidesToShow: 1,
         slidesToScroll: 1
