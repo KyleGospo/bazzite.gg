a15e5220fbfb28eaadc69879298943ab44d456f2
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-04-17 10:26:00 -0700

    Move MFN to a script for CSP

---
+++ wp-content/themes/betheme-child/mfn.js
@@ -0,0 +1,44 @@
+var mfn = {
+  "mobileInit": "1240",
+  "parallax": "translate3d",
+  "responsive": "1",
+  "sidebarSticky": "",
+  "lightbox": {
+    "disable": false,
+    "disableMobile": false,
+    "title": false
+  },
+  "slider": {
+    "blog": 0,
+    "clients": 0,
+    "offer": 0,
+    "portfolio": 0,
+    "shop": 0,
+    "slider": 0,
+    "testimonials": 0
+  },
+  "livesearch": {
+    "minChar": 3,
+    "loadPosts": 10,
+    "translation": {
+      "pages": "Pages",
+      "categories": "Categories",
+      "portfolio": "Portfolio",
+      "post": "Posts",
+      "products": "Products"
+    }
+  },
+  "accessibility": {
+    "translation": {
+      "headerContainer": "Header container",
+      "toggleSubmenu": "Toggle submenu"
+    }
+  },
+  "home_url": "",
+  "home_url_lang": "https:\/\/bazzite.gg",
+  "site_url": "https:\/\/bazzite.gg",
+  "translation": {
+    "success_message": "Link copied to the clipboard.",
+    "error_message": "Something went wrong. Please try again later!"
+  }
+};
